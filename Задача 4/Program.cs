08a82251f22cd42bad3e86ad1915198273c6acab
IrinaKaganovskaia <Kaganovskaia@icloud.com>
2022-07-03 17:09:48 +0300

    Сделална задача 4. Но там есть лаг, не могу понять почему

---
+++ Задача 4/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Напишите программу, которая на вход принимает три числа 
+и выдает максимальное из этих чисел*/
+int number1;
+int number2;
+int number3;
+Console.WriteLine("Введите первое число ");
+number1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+number2=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число ");
+number3=Convert.ToInt32(Console.ReadLine());
+if (number1>number2,number1>number3);
+Console.WriteLine()
+
