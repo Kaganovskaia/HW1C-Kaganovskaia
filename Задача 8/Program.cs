3b5d9da0f19b340fa5d3ab8a4c08fd68e1d4e77c
IrinaKaganovskaia <Kaganovskaia@icloud.com>
2022-07-03 20:06:01 +0300

    Со вторй попытки задача 8 решена

---
+++ Задача 8/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.*/
+int number=20;
+int count=0;
+while (count<=number)
+{
+    Console.WriteLine(count+ " ");
+
+    count+=2;
+   
+}
+Console.WriteLine();
